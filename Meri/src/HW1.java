44c6d164c58e61efb4f9718348820bd6d0e759e7
Khanhtoichoi <166400485+Khanhtoichoi@users.noreply.github.com>
2024-04-08 20:22:01 +0700

    Meri

---
+++ Meri/src/HW1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import  java.lang.String;
+public class HW1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        Integer x = Integer.parseInt(s);
+        System.out.print(x);
+    }
+}
